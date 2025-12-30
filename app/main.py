b1b29c66ea79986b8b28acc8177895f560f77cdf
Hugo Romero <hugomrj@gmail.com>
2025-12-29 22:10:04 -0300

    29dic 2210

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
+import os
 from fastapi import FastAPI
 from app.routes import router
 
-app = FastAPI(title="Factum")
+ROOT_PATH = os.getenv("ROOT_PATH", "")
+
+app = FastAPI(
+    title="Factum",
+    root_path=ROOT_PATH
+)
 
 app.include_router(router)
+
