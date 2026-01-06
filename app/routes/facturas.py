6ab1a63128876e382a31526f397d15665a38c7ed
Hugo Romero <hugomrj@gmail.com>
2026-01-05 23:07:44 -0300

    5ene2307

--- app/routes/facturas.py
+++ app/routes/facturas.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Body, Header
 from app.services.proxy import forward
-
+from fastapi import Response
 
 router = APIRouter()
 
@@ -8,3 +8,25 @@ router = APIRouter()
 def factura_async(emisor: str, body: dict = Body(...), token: str | None = Header(None)):
     resp = forward("POST", "/api/factura/async/recibe", data=body, token=token)
     return resp.json()
+
+
+
+
+@router.post("/{emisor}/factura/xml/generar")
+def generar_factura_xml(
+    emisor: str,
+    body: dict = Body(...),
+    token: str | None = Header(None)
+):
+    resp = forward(
+        "POST",
+        "/factura/xml/generar",
+        data=body,
+        token=token
+    )
+
+    return Response(
+        content=resp.text,
+        status_code=resp.status_code,
+        media_type="text/plain"
+    )
