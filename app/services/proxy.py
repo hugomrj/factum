c3007c4407b551f032391391c27849ca5856e5af
Hugo Romero <hugomrj@gmail.com>
2026-03-10 00:06:48 -0300

    forward

--- app/services/proxy.py
+++ app/services/proxy.py
@@ -1,14 +1,15 @@
 import requests
 from requests import Response
 from config import API_DESTINO
-
+º
 
 def forward(
     method: str,
     endpoint: str,
-    *,
+    *,  # ← Esto obliga a usar nombres de argumentos
     params: dict | None = None,
     data: dict | None = None,
+    headers: dict | None = None, 
     token: str | None = None
 ) -> Response:
     method = method.upper()
